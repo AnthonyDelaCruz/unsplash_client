87b53a7820ab19c4a79ca994b33d3d971bd494a8
Anthony Dela Cruz <anthony@pushfit.tv>
2020-02-16 04:03:02 +0800

    used env instead of publicruntimeconfig for build

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-import getConfig from "next/config";
 import Unsplash from "unsplash-js";
 import axios from "axios";
 
-const { publicRuntimeConfig } = getConfig();
+
 const axiosInstance = axios.create({});
 // must instantiate fetch globally for unsplash-js to work
 global.fetch = axiosInstance;
 
-const unsplashAccessKey = env.UNSPLASH_ACCESS_KEY;
+const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY;
 
 export const unsplashInstance = new Unsplash({
   accessKey: unsplashAccessKey
