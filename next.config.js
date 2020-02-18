839e7d1ec762edf85bc876ea594ba85883036bca
Anthony Dela Cruz <anthony@pushfit.tv>
2020-02-18 17:08:49 +0800

    ommited next.config.js in gitignore

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env: {
+    UNSPLASH_ACCESS_KEY: "5-aHnDMaj8o3JR_ydW87r_gvQg21GCpr9wzxfu3LHks",
+    UNASPASH_SECRET_KEY: "y9RLE7c33XBI-1dpMj2ooinKAK5OUnzm3t0g6AjmuGI"
+  }
+};
