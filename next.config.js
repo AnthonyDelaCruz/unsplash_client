fb500fb889a9a5b1200b16d779fa27c4dd8f2fab
Anthony Dela Cruz <anthony@pushfit.tv>
2020-02-22 01:21:05 +0800

    added text to next js config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ module.exports = {
     ACCESS_KEY: process.env.ACCESS_KEY,
     DOMAIN:
       process.env.NODE_ENV === "production"
-        ? "unsplash-client.now.sh"
-        : "localhost:3000",
+        ? "http://unsplash-client.now.sh"
+        : "http://localhost:3000",
     FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID
   }
 };
