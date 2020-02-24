d17dcec5e3a0edb513165bade2a9ef4b35b990c5
Anthony Dela Cruz <anthony@pushfit.tv>
2020-02-25 02:10:29 +0800

    added framer motion for page transition animations

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,12 @@
+import { motion } from "framer-motion";
 import Nav from "./nav";
 import Sidebar from "./Sidebar";
+
+const pageVariants = {
+  initial: { opacity: 0 },
+  enter: { opacity: 1, transition: { duration: 0.5 } },
+  exit: { opacity: 0, transition: { duration: 0.2 } }
+};
 
 export default function Layout({ children }) {
   return (
@@ -8,7 +15,16 @@ export default function Layout({ children }) {
       <div className="container-fluid">
         <div className="row">
           <Sidebar customClassName="col-md-3 d-none d-md-flex flex-column justify-content-around" />
-          <div className="col-md-9 col-sm-12 p-0">{children}</div>
+          <div className="col-md-9 col-sm-12 p-0">
+            <motion.div
+              initial="initial"
+              animate="enter"
+              exit="exit"
+              variants={pageVariants}
+            >
+              {children}
+            </motion.div>
+          </div>
         </div>
       </div>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../public/styles.css";
+import { AnimatePresence } from "framer-motion";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps, router }) {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Component {...pageProps} key={router.route} />
+    </AnimatePresence>
+  );
 }
