373dc930c10c2569a69aaa479932b333db47ebcc
Anthony Dela Cruz <anthony@pushfit.tv>
2020-02-29 02:17:25 +0800

    migrated styles of photo details page in css file

--- pages/photo/[id].js
+++ pages/photo/[id].js
@@ -14,6 +14,7 @@ import Layout from "../../components/MainLayout";
 import CollectionCards from "../../components/CollectionCards";
 import { axiosInstance } from "../../config";
 
+import styles from "../../public/pageStlyes/photoDetails.module.css";
 /**
  * @TODO
  * SEO tags
@@ -39,130 +40,105 @@ export default function Photo({ id, photoDetails }) {
     "related_collections.results",
     []
   );
+
   return (
     <Layout withOutSidebar>
-      <div>
-        <div className="px-2 px-md-5 py-3">
-          <Link href="/">
-            <a className="backToHome">
-              <IoIosArrowRoundBack size="1.875rem" /> Back to Home
-            </a>
-          </Link>
-        </div>
-        <div className="photo d-flex flex-column-reverse flex-md-row justify-content-md-center pt-md-5 pb-md-5 pt-sm-0">
-          <div className="info-section-container mt-4 mt-md-0 px-3 px-md-5">
-            <div className="d-flex align-items-center info-section">
-              <Img
-                className="h-auto img img-fluid"
-                src={userAvatarUrl}
-                alt={userName}
-              />
-              <h3 className="mx-3 mb-0">{userName}</h3>
+      <div className="px-2 px-md-5 py-3">
+        <Link href="/">
+          <a className={`${styles.backToHome}`}>
+            <IoIosArrowRoundBack size="1.875rem" /> Back to Home
+          </a>
+        </Link>
+      </div>
+      <div className="photo d-flex flex-column-reverse flex-md-row justify-content-md-center pt-md-5 pb-md-5 pt-sm-0">
+        <div
+          className={`${styles.infoSectionContainer} mt-4 mt-md-0 px-3 px-md-5`}
+        >
+          <div className="d-flex align-items-center info-section">
+            <Img
+              className="h-auto img img-fluid"
+              src={userAvatarUrl}
+              alt={userName}
+            />
+            <h3 className="mx-3 mb-0">{userName}</h3>
+          </div>
+          <div className="my-4 my-md-3 d-flex justify-content-between">
+            <div className="mx-1 d-flex align-items-center flex-column flex-md-row">
+              <div className="d-flex">
+                <IoIosEye size="22px" className="mr-1 mr-md-2" />
+                <span className="mr-2">Views</span>
+              </div>
+              {viewCount}
             </div>
-            <div className="my-4 my-md-3 d-flex justify-content-between">
-              <div className="mx-1 d-flex align-items-center flex-column flex-md-row">
-                <div className="d-flex">
-                  <IoIosEye size="22px" className="mr-1 mr-md-2" />
-                  <span className="mr-2">Views</span>
-                </div>
-                {viewCount}
+            <div className="mx-1 d-flex align-items-center flex-column flex-md-row">
+              <div>
+                <IoIosHeart size="20px" className="mr-1 mr-md-2" />
+                <span className="mr-2">Likes</span>
               </div>
-              <div className="mx-1 d-flex align-items-center flex-column flex-md-row">
-                <div>
-                  <IoIosHeart size="20px" className="mr-1 mr-md-2" />
-                  <span className="mr-2">Likes</span>
-                </div>
-                {likeCount}
-              </div>
-              <div className="mx-1 d-flex align-items-center flex-column flex-md-row">
-                <div>
-                  <IoIosCloudDownload size="20px" className="mr-1 mr-md-2" />
-                  <span className="mr-2">Downloads</span>
-                </div>
-                {downloadCount}
-              </div>
+              {likeCount}
             </div>
-            <div className="info-section mt-3">
+            <div className="mx-1 d-flex align-items-center flex-column flex-md-row">
+              <div>
+                <IoIosCloudDownload size="20px" className="mr-1 mr-md-2" />
+                <span className="mr-2">Downloads</span>
+              </div>
+              {downloadCount}
+            </div>
+          </div>
+          <div className="info-section mt-3">
+            <div>
+              <p>
+                Created:{" "}
+                <span className="text-muted">
+                  <Moment format="YYYY/MM/DD">{updatedAtDate}</Moment>
+                </span>
+              </p>
+            </div>
+            {photoDescription && (
               <div>
                 <p>
-                  Created:{" "}
-                  <span className="text-muted">
-                    <Moment format="YYYY/MM/DD">{updatedAtDate}</Moment>
-                  </span>
+                  About the photo:{" "}
+                  <span className="text-muted">{photoDescription}</span>
                 </p>
               </div>
-              {photoDescription && (
-                <div>
-                  <p>
-                    About the photo:{" "}
-                    <span className="text-muted">{photoDescription}</span>
-                  </p>
-                </div>
-              )}
+            )}
+            <div>
+              <p>Tags</p>
               <div>
-                <p>Tags</p>
-                <div>
-                  {tags.map(tag => (
-                    <Link href="/">
-                      <a className="tags btn btn-outline-danger mr-2 mb-2 text-muted">
-                        {tag.title}
-                      </a>
-                    </Link>
-                  ))}
-                </div>
+                {tags.map(tag => (
+                  <Link href="/">
+                    <a
+                      className={`${styles.tags} btn btn-outline-danger mr-2 mb-2 text-muted`}
+                    >
+                      {tag.title}
+                    </a>
+                  </Link>
+                ))}
               </div>
             </div>
           </div>
-          <div className="mb-4 mb-md-0 text-center">
-            <Img
-              className="feature-photo h-auto img img-fluid"
-              src={photoImgUrl}
-              alt={photoAltDescription}
-            />
-          </div>
         </div>
-        <div className="related-section text-center my-5">
-          <h3>
-            Related <span className="font-weight-bold">Collections.</span>
-          </h3>
-          <div className="container">
-            <div className="my-5 row">
-              {!_isEmpty(relatedCollections) &&
-                relatedCollections.map((collection, i) => (
-                  <CollectionCards collection={collection} key={i} />
-                ))}
-            </div>
+        <div className="mb-4 mb-md-0 text-center">
+          <Img
+            className="feature-photo h-auto img img-fluid"
+            src={photoImgUrl}
+            alt={photoAltDescription}
+          />
+        </div>
+      </div>
+      <div className={`${styles.relatedSection} text-center my-5`}>
+        <h3>
+          Related <span className="font-weight-bold">Collections.</span>
+        </h3>
+        <div className="container">
+          <div className="my-5 row">
+            {!_isEmpty(relatedCollections) &&
+              relatedCollections.map((collection, i) => (
+                <CollectionCards collection={collection} key={i} />
+              ))}
           </div>
         </div>
       </div>
-      <style jsx>{`
-        .info-section-container {
-          max-width: 37.5rem;
-        }
-        .tags {
-          border-color: #cdcdcd;
-        }
-        .tags:hover {
-          color: #black !important;
-          background: transparent;
-        }
-        p {
-          font-size: 1.25rem;
-        }
-        .related-section {
-          margin-top: 5rem;
-        }
-        .backToHome {
-          color: black;
-          font-size: 1rem;
-          text-decoration: none;
-        }
-        @media only screen and (max-width: 768px) {
-          .image-section-container img {
-            border
-          }
-        }
-      `}</style>
     </Layout>
   );
 }
